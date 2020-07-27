3384c3da0342f01f615da3019b3ffbfcbda93952
KIMTANGGU <xodus1993@gmail.mail>
2020-07-27 19:25:08 +0900

    adasd

--- classB06/js/script_ui.js
+++ classB06/js/script_ui.js
@@ -2,20 +2,28 @@ $(function(){
     //첫 화면 세팅 버튼
     $(".btn_setting").click(function(){
         $(this).parent().hide();
-        settingFn();
+        loadDataFn();
     });
 
     //테스트를 위한 임시 호출
-    settingFn()
+    loadDataFn()
      
-    function settingFn(){
+    var complateData; //json 데이터 변수
+    function loadDataFn(){
         $(".section.reservation").show();
         $.ajax({
             url:"js/data.json",
             dataType:"json",
             success: function(result){
-                console.log(result);
+                //console.log(result);
+                complateData = result.seatInfo;
+                settingSeatFn();
             }
         })
+    }
+
+    function settionSearFn(){
+        //파싱작업
+        console.log(complateData.length)
     }
 })
