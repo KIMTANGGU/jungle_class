4dc645476e1311826b2ad34924f30cf671097b73
KIMTANGGU <xodus1993@gmail.mail>
2020-07-27 19:19:28 +0900

    aaa

--- classB06/js/script_ui.js
+++ classB06/js/script_ui.js
@@ -1,4 +1,21 @@
 $(function(){
-    
-    
+    //첫 화면 세팅 버튼
+    $(".btn_setting").click(function(){
+        $(this).parent().hide();
+        settingFn();
+    });
+
+    //테스트를 위한 임시 호출
+    settingFn()
+     
+    function settingFn(){
+        $(".section.reservation").show();
+        $.ajax({
+            url:"js/data.json",
+            dataType:"json",
+            success: function(result){
+                console.log(result);
+            }
+        })
+    }
 })
