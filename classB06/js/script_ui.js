bc456704dbbf3175832c2929310a52330bb24f82
KIMTANGGU <xodus1993@gmail.mail>
2020-07-27 19:40:25 +0900

    aaaa

--- classB06/js/script_ui.js
+++ classB06/js/script_ui.js
@@ -8,19 +8,19 @@ $(function(){
     //테스트를 위한 임시 호출
     loadDataFn()
      
-    var complateData; //json 데이터 변수
+    var complateData; //json 데이터 담는변수
     function loadDataFn(){
         $(".section.reservation").show();
         $.ajax({
             url:"js/data.json",
             dataType:"json",
             success: function(result){
-                //console.log(result);
+                //consold.log(result);
                 complateData = result.seatInfo;
                 settingSeatFn();
             }
         })
-    }
+    };
 
     function settionSearFn(){
         //파싱작업
